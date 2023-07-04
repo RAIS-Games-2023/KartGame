f3cf8f2ac8345a6d1ca7639da78c3429f019f894
PinHeadLarry940 <tylerm1001@gmail.com>
2023-07-04 12:53:34 -0600

    new input system

--- Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
+++ Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -1,19 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace KartGame.KartSystems {
 
     public class KeyboardInput : BaseInput
     {
-        public string TurnInputName = "Horizontal";
-        public string AccelerateButtonName = "Accelerate";
-        public string BrakeButtonName = "Brake";
+        /*       public string TurnInputName = "Horizontal";
+               public string AccelerateButtonName = "Accelerate";
+               public string BrakeButtonName = "Brake";*/
+
+        public PlayerInput input;
+
+        bool accelerate, brake;
+        float turn_input;
+
+        private void Awake()
+        {
+            input = GetComponent<PlayerInput>();
+        }
 
         public override InputData GenerateInput() {
+
+            accelerate = input.actions.FindAction("Accelerate").IsPressed();
+            brake = input.actions.FindAction("Brake").IsPressed();
+            turn_input = input.actions.FindAction ("TurnInput").ReadValue<Vector2>().x;
+
             return new InputData
             {
-                Accelerate = Input.GetButton(AccelerateButtonName),
-                Brake = Input.GetButton(BrakeButtonName),
-                TurnInput = Input.GetAxis("Horizontal")
+                Accelerate = accelerate,
+                Brake = brake,
+                TurnInput = turn_input
             };
         }
     }
